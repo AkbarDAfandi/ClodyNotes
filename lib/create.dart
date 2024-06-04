fc1fe082e522b74e566e05b9f3fc4e165005d84c
Akbar Afandi <akbardafandi@gmail.com>
2024-06-04 17:46:06 +0700

    Bug fix

--- lib/create.dart
+++ lib/create.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:cloudynotesv3/home.dart';
+
 
 final supabase = Supabase.instance.client;
 
